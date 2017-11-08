d7c3c8937e375cc2f1266f4ee0dfc1163b91e611
Brian Winter <blackpawn8990@gmail.com>
2017-11-08 03:06:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ const newID = oldID + 1000000000;
 
 const ageIsValid = Number.isInteger(currentAge);
 
-const randomNumber = Math.random() + 20;
+const randomNumber = Math.floor(Math.random() + 20);
